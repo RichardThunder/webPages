46a9efbb997fd700e911078c8adefbe4411f6734
Richard <yuetingpei888@gmail.com>
2024-01-15 02:53:29 +0800

    refactor

--- basic/piggame/script.js
+++ basic/piggame/script.js
@@ -2,24 +2,31 @@
  * @Author: Richard yuetingpei888@gmail.com
  * @Date: 2024-01-14 23:00:55
  * @LastEditors: Richard yuetingpei888@gmail.com
- * @LastEditTime: 2024-01-15 01:53:15
+ * @LastEditTime: 2024-01-15 02:52:11
  * @FilePath: \webPages\basic\piggame\script.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 'use strict';
-let totalScore1 = 0;
-let totalScore2 = 0;
-let currentScore1 = 0;
-let currentScore2 = 0;
+let totalScores=[0,0];
+/* let currentScore1 = 0;
+let currentScore2 = 0; */
+let currentScore=0;
 let diceScore = 0;
-let currentPlayer = 1;
+let currentPlayer = 0;
 const diceEl = document.querySelector('.dice');
+/* const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1'); */
+const play0El = document.querySelector('.player--0');
+const play1El = document.querySelector('.player--1');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 diceEl.classList.add('hidden');
 const rollDice = function () {
   console.log('invoke rollDice');
   let score = Math.trunc(Math.random() * 6 + 1);
   console.log(score);
-  switch (score) {
+  diceEl.src=`dice-${score}.png`;
+  /* switch (score) {
     case 1:
       document.querySelector('img').src = 'dice-1.png';
       break;
@@ -40,45 +47,56 @@ const rollDice = function () {
       break;
     default:
       break;
-  }
+  } */
 
   if (diceEl.classList.contains('hidden')) diceEl.classList.remove('hidden');
-  if (currentPlayer === 1) {
+  /* if (currentPlayer === 1) {
     //将分数加到对应玩家, 如果是1,那么交换控制,更改样式,将得分清零
     if (score === 1) {
       currentScore1 = 0;
-      document.getElementById('current--0').textContent = currentScore1;
+      current0.textContent = currentScore1;
       currentPlayer = 2;
       let sec = document.querySelectorAll('section');
       sec[0].classList.remove('player--active');
       sec[1].classList.add('player--active');
     } else {
       currentScore1 += score;
-      document.getElementById('current--0').textContent = currentScore1;
+      current0.textContent = currentScore1;
     }
   } else if (currentPlayer === 2) {
     if (score === 1) {
       currentScore2 = 0;
-      document.getElementById('current--1').textContent = currentScore2;
+      current1.textContent = currentScore2;
       currentPlayer = 1;
       let sec = document.querySelectorAll('section');
       sec[1].classList.remove('player--active');
       sec[0].classList.add('player--active');
     } else {
       currentScore2 += score;
-      document.getElementById('current--1').textContent = currentScore2;
+      current1.textContent = currentScore2;
     }
+  } */
+  if (score === 1) {
+    currentScore=0;
+    document.getElementById(`current--${currentPlayer}`).textContent = 0;
+    currentPlayer = currentPlayer===0 ? 1:0;
+    play0El.classList.toggle('player--active');
+    play1El.classList.toggle('player--active');
+  }
+  else{
+    currentScore += score;
+    document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
   }
 };
 //更新total后做判断胜利, 如果胜利那么就不清零 否则清零并交换控制
 const hold = function () {
   console.log('invoke hold');
-  if (currentPlayer === 1) {
+ /*  if (currentPlayer === 1) {
     totalScore1 += currentScore1;
     document.getElementById('score--0').textContent = totalScore1;
     if (isWin()) return;
     currentScore1 = 0;
-    document.getElementById('current--0').textContent = currentScore1;
+    current0.textContent = currentScore1;
 
     currentPlayer = 2;
     let sec = document.querySelectorAll('section');
@@ -89,23 +107,31 @@ const hold = function () {
     document.getElementById('score--1').textContent = totalScore2;
     if (isWin()) return;
     currentScore2 = 0;
-    document.getElementById('current--1').textContent = currentScore2;
+    current1.textContent = currentScore2;
 
     currentPlayer = 1;
     let sec = document.querySelectorAll('section');
     sec[1].classList.remove('player--active');
     sec[0].classList.add('player--active');
-  }
+  } */
+    totalScores[currentPlayer]+=currentScore
+    document.getElementById(`score--${currentPlayer}`).textContent = totalScores[currentPlayer];
+    if (isWin()) return;
+    currentScore=0;
+    document.getElementById(`current--${currentPlayer}`).textContent=0;
+    currentPlayer = currentPlayer === 0 ? 1:0;
+    play0El.classList.toggle('player--active');
+    play1El.classList.toggle('player--active');
 };
 
 const reset = function () {
   console.log('invoke reset');
-  currentScore1 = 0;
+ /*  currentScore1 = 0;
   currentScore2 = 0;
   totalScore1 = 0;
   totalScore2 = 0;
-  document.getElementById('current--0').textContent = currentScore2;
-  document.getElementById('current--1').textContent = currentScore2;
+  current0.textContent = currentScore2;
+  current1.textContent = currentScore2;
   document.getElementById('score--0').textContent = totalScore1;
   document.getElementById('score--1').textContent = totalScore2;
   diceScore = 0;
@@ -116,11 +142,23 @@ const reset = function () {
   let btns = document.querySelectorAll('button');
   btns[1].disabled = false;
   btns[2].disabled = false;
-  diceEl.classList.add('hidden');
+  diceEl.classList.add('hidden'); */
+    currentScore=0;
+    totalScores=[0,0];
+    currentPlayer=0;
+    document.getElementById('current--0').textContent =0;
+    document.getElementById('current--1').textContent =0;
+    document.getElementById('score--0').textContent=0;
+    document.getElementById('score--1').textContent=0;
+    play0El.className = 'player player--0 player--active';
+    play1El.className = 'player player--1';
+    btnHold.disabled = false;
+    btnRoll.disabled = false;
+    diceEl.classList.add('hidden');
 };
 //判断胜利并禁用按钮
 const isWin = function () {
-  let btns = document.querySelectorAll('button');
+  /* let btns = document.querySelectorAll('button');
   let sec = document.querySelectorAll('section');
   if (totalScore1 >= 100) {
     sec[0].classList.add('player--winner');
@@ -133,6 +171,13 @@ const isWin = function () {
     btns[2].disabled = true;
     return true;
   }
+  return false; */
+  if(document.getElementById(`score--${currentPlayer}`).textContent >= 20)
+  {document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
+  btnHold.disabled=true;
+  btnRoll.disabled=true;
+  return true;}
+  else
   return false;
 };
 
