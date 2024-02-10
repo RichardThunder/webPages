b14ea2da169a2a76db2baabfd73580b9f681a1a1
Richard <yuetingpei888@gmail.com>
2024-02-10 22:08:46 +0800

    array method

--- basic/function/final/script.js
+++ basic/function/final/script.js
@@ -624,37 +624,49 @@ GOOD LUCK 😀
 // (()=>console.log('This will Also never run again!'
 // ))();
 
-let f;
+// let f;
 
-const g=function(){
-    const a=23;
-    f=function(){
-        console.log(a*2);  
-    };
-};
+// const g=function(){
+//     const a=23;
+//     f=function(){
+//         console.log(a*2);  
+//     };
+// };
 
-const h=function(){
-    const b=777;
-    f=function(){
-        console.log(b*2);  
-    };
-};
-g();
-f();
-console.dir(f);
+// const h=function(){
+//     const b=777;
+//     f=function(){
+//         console.log(b*2);  
+//     };
+// };
+// g();
+// f();
+// console.dir(f);
 
-h();
-f();
-console.dir(f)
+// h();
+// f();
+// console.dir(f)
 
-//example 2 
-const boardPassengers=function(n,wait){
-    const perGroup=n/3;
-    setTimeout(function(){},1000);
-    console.log(`Will start boarding in ${wait} seconds`);  
-};
+// //example 2  timer
+// // perGroup 就是闭包
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers.`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers.`);
+//   }, wait * 1000);
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
 
-setTimeout(function(){
-    console.log('TIMER');
-    
-},1000);
+// boardPassengers(180, 3);
+
+
+//code challenge
+(function(){
+    const header=document.querySelector('h1')
+    header.style.color='red'
+    document.querySelector('body').addEventListener('click',function(){
+        header.style.color='blue'
+        
+    })
+})();
