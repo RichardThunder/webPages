933635ac0b8a952b98ff81e57d353060dd2c22f6
Richard <yuetingpei888@gmail.com>
2024-02-16 22:30:00 +0800

    close

--- basic/11-Arrays-Bankist/starter/script.js
+++ basic/11-Arrays-Bankist/starter/script.js
@@ -2,7 +2,7 @@
  * @Author: Richard yuetingpei888@gmail.com
  * @Date: 2024-02-10 13:16:22
  * @LastEditors: Richard yuetingpei888@gmail.com
- * @LastEditTime: 2024-02-16 00:59:54
+ * @LastEditTime: 2024-02-16 22:27:42
  * @FilePath: /webPages/basic/11-Arrays-Bankist/starter/script.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -225,10 +225,10 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movement) {
+const displayMovements = function (acc) {
   containerMovements.innerHTML = '';
 
-  movement.forEach(function (value, i) {
+  acc.movements.forEach(function (value, i) {
     const type = value > 0 ? 'deposit' : 'withdrawal';
     if (value < 0) {
       value = Math.abs(value);
@@ -246,36 +246,45 @@ const displayMovements = function (movement) {
   });
 };
 
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, move) => acc + move, 0);
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, move) => acc + move, 0);
-  labelBalance.textContent = balance + '€';
+  labelBalance.textContent = acc.balance + '€';
 };
 
 const calcDisplaySummary = function (account) {
-  const deposits = account.movements
+  account.deposits = account.movements
     .filter(value => value > 0)
     .reduce((acc, curr) => acc + curr, 0);
-  const withdrawals = account.movements
+
+  account.withdrawals = account.movements
     .filter(value => value < 0)
     .reduce((acc, curr) => acc + Math.abs(curr), 0);
-  const interest = account.movements
+  account.interest = account.movements
     .filter(value => value > 0)
     .map((value, index, arr) => (value * account.interestRate) / 100)
     .filter(value => value >= 1)
     .reduce((acc, curr) => acc + curr, 0);
-  labelSumIn.textContent = `${deposits}€`;
-  labelSumOut.textContent = `${withdrawals}€`;
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumIn.textContent = `${account.deposits}€`;
+  labelSumOut.textContent = `${account.withdrawals}€`;
+  labelSumInterest.textContent = `${account.interest}€`;
+};
+let currentAccount;
+const updateUI=function(acc){
+  //displayBalance
+  calcDisplayBalance(acc);
+  //displaySummary
+  calcDisplaySummary(acc);
+  displayMovements(acc);
 };
 
-//find 方法
+//find 方法 登录
 btnLogin.addEventListener('click', function (e) {
   //Prevent form from submitting
   e.preventDefault();
   console.log('LOGIN');
-  const currentAccount = accounts.find(
-    value => value.owner === inputLoginUsername.value
+  currentAccount = accounts.find(
+    value => value.username === inputLoginUsername.value
   );
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
     //Display ui and message
@@ -283,18 +292,82 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+    //清楚input输入框
     inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur(); //隐藏闪烁的光标
-    //displayBalance
-    calcDisplayBalance(currentAccount.movements);
-    //displaySummary
-    calcDisplaySummary(currentAccount);
-    displayMovements(currentAccount.movements);
+    inputLoginPin.blur(); //隐藏PIN处闪烁的光标
+    updateUI(currentAccount);
   }
 });
 
+//转账功能
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(`transfer money`);
+  const recvAccount = accounts.find(
+    account => account.username === inputTransferTo.value
+  );
+  const recvAmount = Number(inputTransferAmount.value);
+  if (
+    recvAmount > 0 &&
+    recvAccount&&
+    recvAmount <= currentAccount.balance &&
+    recvAccount?.username !== currentAccount.username
+  ) {
+    console.log(`transfer valid`);
+    currentAccount.movements.push(-recvAmount);
+    recvAccount.movements.push(recvAmount);
+    updateUI(currentAccount);
+    console.log(recvAccount.movements);
+  } else {
+    console.log(`transfer failed`);
+  }
+  inputTransferTo.value = inputTransferAmount.value = '';
+});
+
+//关闭账户
+btnClose.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log(`Delete`);
+    //验证
+    const closeAccountIndex=accounts.findIndex(acc=>(acc.username===inputCloseUsername.value 
+        && acc.pin===Number(inputClosePin.value)));
+
+    if(closeAccountIndex!=-1){
+        accounts.splice(closeAccountIndex,1);
+        console.log(`account deleted`);
+        currentAccount='';
+        containerApp.style.opacity=0;
+    }else{
+        console.log(`account not deleted`);
+    }
+    //清空输入框
+    inputCloseUsername.value = inputClosePin.value='';
+});
+
+//loan
+btnLoan.addEventListener('click',function(e){
+    e.preventDefault();
+    const amount=Number(inputLoanAmount.value);
+    if(amount>0&& currentAccount.movements.some(mov=>mov>=amount*0.1)){
+        //add movement
+        currentAccount.movements.push(amount);
+        //update UI
+        updateUI(currentAccount);
+    }
+    inputLoanAmount='';
+})
+
+
+console.log(account1.movements);
+//include
+console.log(account1.movements.includes(-130));
+//some
+console.log(account1.movements.some(mov=>mov>0));
+//every
+console.log(account1.movements.every(mov=>mov>10000));
+
+
 //使用map过滤元素
-const user = 'Steven Thomas Williams';
 const createUsernames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
